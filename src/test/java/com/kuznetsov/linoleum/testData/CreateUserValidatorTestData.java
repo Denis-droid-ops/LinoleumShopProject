549371c87a0f427c45e2612513ba4841d73b59e7
Denis-droid-ops <denis.denis.kuznecov@mail.ru>
2023-05-09 19:01:48 +0300

    Commit for other workspace

--- src/test/java/com/kuznetsov/linoleum/testData/CreateUserValidatorTestData.java
+++ src/test/java/com/kuznetsov/linoleum/testData/CreateUserValidatorTestData.java
@@ -9,5 +9,10 @@ import java.util.List;
 public final class CreateUserValidatorTestData{
     public static final Error ERROR_NULL_NAME = new Error("nullable.name","Name is null, please try again!");
     public static final Error ERROR_NULL_EMAIL = new Error("nullable.email","Email is null, please try again!");
+    public static final Error ERROR_NULL_PASSWORD = new Error("nullable.password","Password is null, please try again!");
+    public static final Error ERROR_NULL_PHONE_NUMBER = new Error("nullable.phoneNumber","PhoneNumber is null, please try again!");
     public static final CreateUserDto CREATE_USER_DTO = new CreateUserDto("","","","");
+    public static final CreateUserDto NOTUNIQUE_USER_DTO = new CreateUserDto("Tom","denis.denis.kuznecov@mail.ru","1111","89179292245L");
+    public static final List<Error> NULLABLE_ERRORS = new ArrayList<>(List.of(ERROR_NULL_NAME
+    ,ERROR_NULL_EMAIL,ERROR_NULL_PASSWORD,ERROR_NULL_PHONE_NUMBER));
 }
--- src/test/java/com/kuznetsov/linoleum/validator/CreateUserValidatorTest.java
+++ src/test/java/com/kuznetsov/linoleum/validator/CreateUserValidatorTest.java
@@ -1,16 +1,40 @@
 package com.kuznetsov.linoleum.validator;
 
 import com.kuznetsov.linoleum.dao.UserDao;
-import com.kuznetsov.linoleum.testData.CreateUserValidatorTestData;
+import com.kuznetsov.linoleum.util.ConnectionManager;
+import com.kuznetsov.linoleum.util.InitDB;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static com.kuznetsov.linoleum.testData.CreateUserValidatorTestData.*;
 
 public class CreateUserValidatorTest {
+    private final Logger logger = LoggerFactory.getLogger(CreateUserValidatorTest.class);
     private final CreateUserValidator createUserValidator = CreateUserValidator.getInstance();
     private final UserDao userDao = UserDao.getInstance();
 
+    @BeforeAll
+    static void init(){
+        ConnectionManager.enableConnForTests();
+        InitDB.init();
+    }
+
     @Test
-    void isValid(){
-        createUserValidator.isValid(CreateUserValidatorTestData.CREATE_USER_DTO);
+    void isValidForNullable(){
+        ValidationResult validationResult = createUserValidator.isValid(CREATE_USER_DTO);
+        List<Error> actual = validationResult.getErrors();
+        logger.info("Actual: {},\n expected: {}",actual, NULLABLE_ERRORS);
+        Assertions.assertThat(actual).usingRecursiveComparison().isEqualTo(NULLABLE_ERRORS);
+    }
+
+    @Test
+    void isValidForNotUnique(){
+        ValidationResult validationResult = createUserValidator.isValid(CREATE_USER_DTO);
 
     }
 }
